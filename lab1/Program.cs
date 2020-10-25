147436f4875bf0c1beafeaf05b03b4aecfbc4c7b
Geshmak <72803353+Geshmak@users.noreply.github.com>
2020-10-25 21:23:25 +0300

    Add files via upload

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -14,29 +14,32 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-        Grid2D def0 = new Grid2D((float)1.0, (float)1.0, 5, 5);
-        V4DataOnGrid def1 = new V4DataOnGrid("first", 1.0, def0);
-        Random rand = new Random();
-        def1.InitRandom(0, 100, rand);
-        Console.WriteLine(def1.ToLongString());
+            Grid2D def0 = new Grid2D((float)1.0, (float)1.0, 5, 5);
+            V4DataOnGrid def1 = new V4DataOnGrid("first", 1.0, def0);
+            Random rand = new Random();
+            def1.InitRandom(0, 100, rand);
+            Console.WriteLine(def1.ToLongString());
+
+            V4DataCollection param =(V4DataCollection) def1; //преобразование типа 
+            Console.WriteLine(param.ToLongString());
             
-
-        V4MainCollection def3 = new V4MainCollection();
-        def3.AddDefaults();
-        Console.WriteLine(def3.ToString());
-        Complex[] obj;
-        foreach (var item in def3)
-            {
-                Console.WriteLine("\n");
-                obj = item.NearMax((float)10);
-                for (int i = 0; i < obj.Length; i++)
+            V4MainCollection def3 = new V4MainCollection();
+            def3.AddDefaults();
+            Console.WriteLine(def3.ToString());
+            Complex[] obj;
+            foreach (var item in def3)
                 {
-                    Console.WriteLine(obj[i].ToString());
+                    Console.WriteLine("\n");
+                    obj = item.NearMax((float)10);
+                    for (int i = 0; i < obj.Length; i++)
+                    {
+                        Console.WriteLine(obj[i].ToString());
+                    }
                 }
-            }
 
-
+            
         }
+        
 
     }
 }
